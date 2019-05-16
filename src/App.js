9e320b44c7764d3a7447b01060ee4ef1d23ccfa8
DESKTOP-4LOB5RB\ductuyen <ductuyenc297@gmail.com>
2019-05-16 23:33:11 +0700

    commit source front end

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Full from './components/Full';
+
+import './scss/style.scss';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <Full>
+
+    </Full>
+  );
+}
+
+export default App;
